370e24f428ab95d8ac3c1542749e0fbd0d77660e
fanusi <stephanetrouve@hotmail.com>
2022-05-03 11:21:35 +0200

    Initial Commit

---
+++ Lootlogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lootlogger
+//
+//  Created by Stéphane Trouvé on 03/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
